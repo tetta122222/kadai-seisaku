d1653511ba430e9ee9283f78c1d795646eee0739
tetta <19s032032@gmail.com>
2021-01-14 12:12:41 +0900

    bz

--- 01-作品制作[HTML CSS JS]/js/str_save.js
+++ 01-作品制作[HTML CSS JS]/js/str_save.js
@@ -202,7 +202,7 @@ var startPos = 1000,winScrollTop = 0;
 $(window).on('scroll',function(){
   
     winScrollTop = $(this).scrollTop();
-    if (winScrollTop <= 500) {
+    if (winScrollTop <= 50) {
         $('.navi').addClass('hide');
     }
     else {
