412e2f4566575f6e0d68975bbeea7aa9cee450db
tetta <19s032032@gmail.com>
2021-01-14 12:17:25 +0900

    fas

--- 01-作品制作[HTML CSS JS]/js/str_save2.js
+++ 01-作品制作[HTML CSS JS]/js/str_save2.js
@@ -193,7 +193,7 @@ getfood();
 var startPos = 0,winScrollTop = 0;
 $(window).on('scroll',function(){
     winScrollTop = $(this).scrollTop();
-    if (winScrollTop <= 500) {
+    if (winScrollTop <= 50) {
         $('.form').addClass('hide');
     } else {
         $('.form').removeClass('hide');
