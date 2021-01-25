3d6ede4e46286ed7b6f567e618182c8d0a442214
19s009it <19s009it@gmail.com>
2021-01-25 12:27:22 +0900

    aff

--- 01-作品制作[HTML CSS JS]/js/top.js
+++ 01-作品制作[HTML CSS JS]/js/top.js
@@ -1,1 +1,8 @@
-
+var windowWidth = $(window).width();
+$(".bt").on('click', function(){
+    $(".right_door").addClass("box");
+    setTimeout(function()  {
+        window.location.href = "select.html";    
+    }, 2000);
+        
+});
