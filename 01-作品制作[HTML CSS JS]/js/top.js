cdaac9e5658b066b473091b56277d89ff3bc5b80
tetta122222 <19s032032@gmail.com>
2021-01-26 13:36:08 +0900

    aaaaaa

--- 01-作品制作[HTML CSS JS]/js/top.js
+++ 01-作品制作[HTML CSS JS]/js/top.js
@@ -1,7 +1,7 @@
  $(".norenn").css('top',$(window).height()/3.8);
  $(".norenn").css('right',$(window).height()/4);
  $(".norenn").css('left',$(window).height()/10);
- $(".right_door").css('left',$(window).height()/4);
+ $(".right_door").css('left',$(window).height()/3.5);
  $(".aaaa").css('left',$(window).height()/25);
  $(".right_door").css('top',$(window).height()/2.7);
  $(".aaaa").css('top',$(window).height()/3.2);
--- 01-作品制作[HTML CSS JS]/js/value.js
+++ 01-作品制作[HTML CSS JS]/js/value.js
@@ -3,3 +3,13 @@ $(".radio").on('click', function(){
     sessionStorage.setItem('value',$(this).val());
     sessionStorage.setItem('value1',$(this).data('value'));
 })
+
+let valu1;
+let value2;
+$(".search").on('click', function(){
+    value1 = sessionStorage.getItem('value');
+    value2 = sessionStorage.getItem('value1');
+    if(value1 === "" || value2 === ""){
+        alert("aaaa");
+    }
+})
