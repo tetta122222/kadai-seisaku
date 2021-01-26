499eb01de802a5cde8624da13fc34138f645de18
tetta122222 <19s032032@gmail.com>
2021-01-26 11:19:37 +0900

    ga;ga

--- 01-作品制作[HTML CSS JS]/js/top.js
+++ 01-作品制作[HTML CSS JS]/js/top.js
@@ -1,3 +1,12 @@
+ $(".norenn").css('top',$(window).height()/3.8);
+ $(".norenn").css('right',$(window).height()/4);
+ $(".norenn").css('left',$(window).height()/10);
+ $(".right_door").css('left',$(window).height()/4);
+ $(".aaaa").css('left',$(window).height()/25);
+ $(".right_door").css('top',$(window).height()/2.7);
+ $(".aaaa").css('top',$(window).height()/3.2);
+
+//$(".right_door").css('top',$(window).height()/5);
 var windowWidth = $(window).width();
 $(".bt").on('click', function(){
     $(".right_door").addClass("box");
