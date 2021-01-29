f332e6f37274d652bfccb74e7fa461252e9c6333
tetta122222 <66706500+tetta122222@users.noreply.github.com>
2021-01-29 10:32:21 +0900

    Update top.js

--- 01-作品制作[HTML CSS JS]/js/top.js
+++ 01-作品制作[HTML CSS JS]/js/top.js
@@ -4,14 +4,13 @@
  $(".right_door").css('left',$(window).height()/3.5);
  $(".aaaa").css('left',$(window).height()/25);
  $(".right_door").css('top',$(window).height()/2.7);
- $(".aaaa").css('top',$(window).height()/3.2); 
+ $(".aaaa").css('top',$(window).height()/3.2);  
+ $(".right_door").removeClass("box");
 
-//$(".right_door").css('top',$(window).height()/5);
 var windowWidth = $(window).width();
 $(".bt").on('click', function(){
     $(".right_door").addClass("box");
     setTimeout(function(){ 
         window.location.href = "select.html";    
       }, 2000);
- $(".right_door").removeClass("box");
 });
